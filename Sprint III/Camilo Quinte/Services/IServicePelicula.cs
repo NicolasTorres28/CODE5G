702708950439f3a0bad67ed93e489f794bc49a54
CamiloQuinteroR <90699489+CamiloQuinteroR@users.noreply.github.com>
2021-09-26 18:09:42 -0500

    Create IServicePelicula.cs

---
+++ Sprint III/Camilo Quinte/Services/IServicePelicula.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using SprintIII_G34.Shared.Entity;
+using System.Collections.Generic;
+namespace SprintIII_G34.Client.Services
+{
+    public interface IServicePelicula
+        {
+            List<Movie> GetMovies();
+            
+
+
+
+        }
+    
+}
