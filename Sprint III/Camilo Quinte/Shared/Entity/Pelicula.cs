61d1bf164c5ff4bf1d26f01a5d10704c4bc68ff5
CamiloQuinteroR <90699489+CamiloQuinteroR@users.noreply.github.com>
2021-09-26 18:06:57 -0500

    Create Pelicula.cs

---
+++ Sprint III/Camilo Quinte/Shared/Entity/Pelicula.cs
@@ -0,0 +1,19 @@
+using System;
+namespace  SprintIII_G34.Shared.Entity
+
+{
+    public class Movie
+    {
+        public String Nombre{get;set;}
+        public String Image{get;set;}
+        public String Genero{get;set;}
+        public String Puntuacion_usuario{get;set;}
+        public String Sinopsis{get;set;}
+        public DateTime Fecha_estreno{get;set;}
+        public String Duracion{get;set;}
+        public String Idioma_original{get;set;}
+        public String Produccion{get;set;}
+        public String Direccion{get;set;}
+    }
+    
+}
